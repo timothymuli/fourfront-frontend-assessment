d0ca1f015f407d8848240ad5bed3ad54e305ce65
Timothy Muli <timothymuli32@gmail.com>
2026-02-25 01:56:44 +0300

    feat: make layout responsive and display selected membership details after email submit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,10 @@
-// simple email validation
+var lastOpened = "";
+
 var form = document.getElementById("infoForm");
 var emailInput = document.getElementById("emailInput");
 var msg = document.getElementById("formMsg");
+var detailsBox = document.getElementById("detailsBox");
 
-if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    var email = emailInput.value;
-
-    // very basic checks
-    if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
-      msg.textContent = "Please enter a valid email.";
-      msg.className = "small mt-2 err";
-      return;
-    }
-
-    msg.textContent = "Email received. Membership details will be sent.";
-    msg.className = "small mt-2 ok";
-  });
-}
-
-// plus/minus toggle for collapse
 var foundation = document.getElementById("foundationDesc");
 var economy = document.getElementById("economyDesc");
 var plus1 = document.getElementById("plus1");
@@ -30,6 +13,7 @@ var plus2 = document.getElementById("plus2");
 if (foundation) {
   foundation.addEventListener("shown.bs.collapse", function () {
     plus1.textContent = "−";
+    lastOpened = "foundation";
   });
 
   foundation.addEventListener("hidden.bs.collapse", function () {
@@ -40,9 +24,59 @@ if (foundation) {
 if (economy) {
   economy.addEventListener("shown.bs.collapse", function () {
     plus2.textContent = "−";
+    lastOpened = "economy";
   });
 
   economy.addEventListener("hidden.bs.collapse", function () {
     plus2.textContent = "+";
+  });
+}
+
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var email = emailInput.value;
+
+    if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+      msg.textContent = "Please enter a valid email.";
+      msg.className = "small mt-2 err";
+      detailsBox.style.display = "none";
+      detailsBox.innerHTML = "";
+      return;
+    }
+
+    if (lastOpened === "") {
+      msg.textContent = "Open a membership first, then click Send.";
+      msg.className = "small mt-2 err";
+      detailsBox.style.display = "none";
+      detailsBox.innerHTML = "";
+      return;
+    }
+
+    msg.textContent = "Thanks! Details are shown below.";
+    msg.className = "small mt-2 ok";
+
+    detailsBox.style.display = "block";
+
+    if (lastOpened === "foundation") {
+      detailsBox.innerHTML =
+        "<b>Foundation Membership</b><br>" +
+        "Email: " + email + "<br><br>" +
+        "- Shares + Mansa-X global fund<br>" +
+        "- Online service is free<br>" +
+        "- Office visit may have extra charges<br><br>" +
+        "Support: timothymuthini@proton.me";
+    }
+
+    if (lastOpened === "economy") {
+      detailsBox.innerHTML =
+        "<b>Economy Membership</b><br>" +
+        "Email: " + email + "<br><br>" +
+        "- Guaranteed rate via Mansa-X global fund<br>" +
+        "- Online service is free<br>" +
+        "- Office visit may have extra charges<br><br>" +
+        "Support: timothymuthini@proton.me";
+    }
   });
 }
